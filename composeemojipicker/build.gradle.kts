d999493b63ceb44da63a6705e9ecccc9b7292bcc
Abhimanyu14 <abhimanyu.n14@gmail.com>
2023-08-19 21:18:06 +0530

    [TECH] Library version upgrade

--- composeemojipicker/build.gradle.kts
+++ composeemojipicker/build.gradle.kts
@@ -59,7 +59,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.Abhimanyu14"
             artifactId = "compose-emoji-picker"
-            version = "1.0.0-alpha07"
+            version = "1.0.0-alpha08"
             afterEvaluate {
                 from(components["release"])
             }
