03f58c55bb5c0e9570e3dfb2a3919f5486641f4f
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-09-20 08:34:43 +0530

    [TECH] removed unused code

--- composeemojipicker/src/main/java/com/makeappssimple/abhimanyu/composeemojipicker/utils/RenderUtil.kt
+++ composeemojipicker/src/main/java/com/makeappssimple/abhimanyu/composeemojipicker/utils/RenderUtil.kt
@@ -1,7 +1,6 @@
 package com.makeappssimple.abhimanyu.composeemojipicker.utils
 
 import androidx.emoji2.text.EmojiCompat
-import com.makeappssimple.abhimanyu.composeemojipicker.Emoji
 
 internal fun isEmojiCharacterRenderable(
     emojiCharacter: String,
@@ -10,23 +9,6 @@ internal fun isEmojiCharacterRenderable(
             EmojiCompat.get().loadState == EmojiCompat.LOAD_STATE_SUCCEEDED &&
             EmojiCompat.get().getEmojiMatch(
                 emojiCharacter,
-                Int.MAX_VALUE
-            ) == EmojiCompat.EMOJI_SUPPORTED
-}
-
-@Deprecated(
-    message = "Use isEmojiCharacterRenderable()",
-    replaceWith = ReplaceWith(
-        expression = "isEmojiCharacterRenderable(emoji.character)",
-    ),
-)
-internal fun isEmojiRenderable(
-    emoji: Emoji,
-): Boolean {
-    return EmojiCompat.isConfigured() &&
-            EmojiCompat.get().loadState == EmojiCompat.LOAD_STATE_SUCCEEDED &&
-            EmojiCompat.get().getEmojiMatch(
-                emoji.character,
                 Int.MAX_VALUE
             ) == EmojiCompat.EMOJI_SUPPORTED
 }
